d66ed99e23a023dec4d1ac3f8ecdd79dfabf2c07
Clive Makamara <cmosh@live.com>
2016-12-19 19:00:59 +0300

    small fix

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-var Telegraf = require('telegraf')
-var TelegrafWit = require('telegraf-wit')
+const Telegraf = require('telegraf')
+const TelegrafWit = require('telegraf-wit')
 
-var app = new Telegraf(process.env.BOT_TOKEN)
-var wit = new TelegrafWit(process.env.WIT_TOKEN)
+// Related wit app: https://wit.ai/dotcypress/smart-home/stories
+const app = new Telegraf("asd")
+const wit = new TelegrafWit("asd")
 
 app.use(Telegraf.memorySession())
 app.use(wit.middleware())
 
-wit.onMessage(function * () {
-  yield this.reply(this.state.wit.message)
+// Message handlers
+wit.on('message', (ctx) => {
+  if (ctx.wit.confidence > 0.01) {
+    return ctx.reply(ctx.wit.message)
+  }
 })
 
-app.startPolling()
+// Action handlers
+wit.on('getRoomTemperature', (ctx) => {
+  ctx.wit.context.room = firstEntityValue(ctx.wit.entities, 'room')
+  ctx.wit.context.roomInfo = `Temperature in ${ctx.wit.context.room} - As usual :)`
+})
+
+app.startPolling(30)
+
+function firstEntityValue (entities, entity) {
+  const val = entities && entities[entity] &&
+  Array.isArray(entities[entity]) &&
+  entities[entity].length > 0 &&
+  entities[entity][0].value
+  if (!val) {
+    return null
+  }
+  return typeof val === 'object' ? val.value : val
+}
